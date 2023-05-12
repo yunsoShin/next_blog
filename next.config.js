1bc656c4c8ead033ebb097094946f34890d066b7
Shin <shin4895@gmail.com>
2023-05-13 05:07:04 +0900

    빌드전 마지막 커밋

--- next.config.js
+++ next.config.js
@@ -3,10 +3,10 @@ const nextConfig = {
 }
 module.exports = {
   // ...
-  output: {
-    // Set your desired output directory
-    directory: 'out',
-    // ...
+  images: {
+    loader: 'imgix',
+    path: '',
+    unoptimized: true,
   },
 };
 
--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export async function getStaticProps() {
 }
 
 export default function Home({ allPostsData }) {
-  console.log(allPostsData);
+
   return (
     <Layout home>
       <Head>
