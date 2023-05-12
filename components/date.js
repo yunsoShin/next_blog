fd7a1803e6eb164fde9a9b08d82264ff97a8d861
Shin <shin4895@gmail.com>
2023-05-13 03:34:47 +0900

    moment upgrade

--- components/date.js
+++ components/date.js
@@ -1,7 +1,11 @@
 import { parseISO, format } from 'date-fns';
 import { ko } from 'date-fns/locale';
+import moment from 'moment';
+
 
 export default function PostDate({ dateString }) {
-  const Strdate = parseISO(dateString);
-  return <time dateTime={dateString}>{format(Strdate, 'LLLL d, yyyy',{ locale: ko })}</time>;
+  const date = moment(dateString);
+  const formattedDate = date.format('LLLL d, yyyy');
+
+  return <time dateTime={dateString}>{formattedDate}</time>;
 }
--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ export default function Home({ allPostsData }) {
             <Link href={`/posts/${id}`}>{title}</Link>
             <br />
             <small className={utilStyles.lightText}>
-              <Date dateString={date} />
+              <PostDate dateString={date} />
             </small>
           </li>
           ))}
